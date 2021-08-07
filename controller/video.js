911407090cd5e1f3dff34a0110ff154e81637bef
Rafael <markesxd@gmail.com>
2021-08-07 09:54:38 -0300

    Terminando o CRUD

--- controller/video.js
+++ controller/video.js
@@ -23,9 +23,51 @@ class Video {
           reject(error);
         } else {
           if(results.length == 0) resolve('Video não encontrado');
-          resolve(results);
+          resolve(results[0]);
         }
       })
+    });
+  }
+
+  post(video){
+    const sql = `INSERT INTO video SET ?`;
+    return new Promise((resolve, reject) => {
+      // if(video.titulo == undefined || video.descricao == undefined || video.url == undefined){
+      //   reject('Todos os campos devem ser preenchidos');
+      // }
+      connection.query(sql, video, (error, results, field) => {
+        if(error){
+          reject(error)
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+
+  patch(id, field){
+    const sql = `UPDATE video SET ? WHERE id=${id}`;
+    return new Promise((resolve, reject) => {
+      connection.query(sql, field, (error, results, fields) => {
+        if(error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+
+  delete(id){
+    const sql = `DELETE FROM video WHERE id=${id}`;
+    return new Promise((resolve, reject) => {
+      connection.query(sql, null, (error, results, field) => {
+        if(error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
     });
   }
 }
